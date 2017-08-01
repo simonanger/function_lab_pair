db52f45da26eddd779abd4498268e6d5bc65f138
Simon Anger <simonanger89@gmail.com>
2017-08-01 17:15:03 +0100

    Started further tests

--- ruby_functions_spec.rb
+++ ruby_functions_spec.rb
@@ -67,7 +67,7 @@ class Functions_Practice < MiniTest::Test
 
   #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
-    #add test code here
+    assert_equal( )
   end
 
   #Given the radius of a sphere calculate the volume
