6cacaaa4d9af7d86e8b630cd00d006b5a3cf49ed
Simon Anger <simonanger89@gmail.com>
2017-08-02 09:54:00 +0100

    Going over homework

---
+++ ruby_functions_practice2.rb
@@ -0,0 +1,15 @@
+def return_10
+    return 10
+end
+
+def add(n1, n2)
+  return n1 + n2
+end
+
+def subtract(n1, n2)
+  return n1 - n2
+end
+
+def length_of_string(string)
+  return string.length()
+end
