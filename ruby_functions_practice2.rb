0a70407ceb1707a0d74c840bbe2376f0a8ed8303
Simon Anger <simonanger89@gmail.com>
2017-08-02 09:59:27 +0100

    Test more

--- ruby_functions_practice2.rb
+++ ruby_functions_practice2.rb
@@ -13,3 +13,16 @@ end
 def length_of_string(string)
   return string.length()
 end
+
+def number_to_full_month_name(month_number)
+  case (month_number)
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+
+  end
+
+end
